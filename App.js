c3f20f75f933d8ed34ac237b2b3283a0072c1c7e
abhijeet011 <kumarabhijeet011@gmail.com>
2025-03-28 22:28:22 +0530

    episode 1 removed unwanted code

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-// const heading = React.createElement('h1', { id: "heading" }, "Hello World from React!");
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// root.render(heading);
-
-//
 const parent = React.createElement('div',
   { id: "parent" }, [React.createElement('div', { id: 'child' },
     [React.createElement('h1', { id: 'heading' }, 'I am an h1 tag'),
@@ -18,6 +10,5 @@ const parent = React.createElement('div',
   )
 ]
 )
-console.log(parent)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
