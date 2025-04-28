a990d72f2f50f7bc4c8bd3d52df958fcbb8b4d4c
abhijeet011 <kumarabhijeet011@gmail.com>
2025-04-28 17:17:58 +0530

    episode 9

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,7 @@ const About = () => {
     <div>
       <h1>About</h1>
       <h2>This is Namaste React Web series one</h2>
-      <div>hi</div>
-      <User name={"Abhijeet Kumar"} location="Bangalore" />
+      {/* <User name={"Abhijeet Kumar"} location="Bangalore" /> */}
       <UserClass
         name={"Abhijeet kumar (class based)"}
         location={"dehradun classs"}
--- src/components/userClass.js
+++ src/components/userClass.js
@@ -4,14 +4,26 @@ class UserClass extends React.Component {
     super(props);
     console.log(props);
     this.state = {
-      count: 0,
+      count: 1,
     };
   }
   render() {
     const { name, location } = this.props;
+    const { count } = this.state;
     return (
       <div className="user-card">
-        <h1></h1>
+        <h1>count: {count}</h1>
+        <button
+          onClick={() => {
+            //Never update state variables Directly
+            //this.state.count=this.state.count+1
+            this.setState((prev) => ({
+              count: prev.count * 2,
+            }));
+          }}
+        >
+          Count Increase
+        </button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact:@ abhiakela</h4>
